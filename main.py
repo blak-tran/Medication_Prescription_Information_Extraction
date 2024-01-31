077bcb3f4cc383a762d7a86896053063797973ea
blak-tran <ceilingdat@gmail.com>
2024-01-31 06:09:38 +0000

    update type

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from fastapi import FastAPI, File
 from app.core.utils import save_Image_from_bytes
 from app.modules.Chatbot_core.chatbot_model import llm_model
 from app.modules.OCR.SCANNER_DATA import app_scanner
-import jwt, base64
+import base64
 
 app = FastAPI()
 
 
 @app.post("/api/v1/predict-ocr/")
-async def predict_ocr(user_Id: str = None, 
-                      prescription_Id: str = None, 
-                      image: bytes= None ):
+async def predict_ocr(data: dict):
+    user_Id = data.get("User_Id")
+    prescription_Id = data.get("Prescription_Id")
+    image = data.get("Image")
+    
     error = ""
     STATUS = 200 
     try:
@@ -29,17 +31,19 @@ async def predict_ocr(user_Id: str = None,
     response = {"status": STATUS, 
                 "user_Id": user_Id, 
                 "prescription_Id": prescription_Id,
-                "data": data, 
+                "data": str(data), 
                 "imageBase64": img_bytes, 
                 "error": error}
+    print(response)
     
     return response
 
 
 @app.post("/api/v1/predict-info/")
-async def predict_info(user_Id: str = None, 
-                      prescription_Id: str = None,
-                      data: str = None):
+async def predict_info(data: dict):
+    user_Id = data.get("User_Id")
+    prescription_Id = data.get("Prescription_Id")
+    data = data.get("data")
     error = ""
     STATUS = 200
 
@@ -52,4 +56,5 @@ async def predict_info(user_Id: str = None,
         print(f"An error occurred: {e}")
 
     response = {"status": STATUS, "user_data": data, "error": error}
+    print(response)
     return response
