9a35d3d8301cf517c21705a480640c0d506abbff
blak-tran <ceilingdat@gmail.com>
2024-02-17 15:13:44 +0700

    update package version

--- convert_image_base64.py
+++ convert_image_base64.py
@@ -12,4 +12,14 @@ base64_image = base64.b64encode(image_data).decode('utf-8')
 with open('base64_image.txt', 'w') as text_file:
     text_file.write(base64_image)
 
-print(base64_image)
+# print(base64_image)
+# import subprocess
+
+# # Run the pip freeze command and capture the output
+# pip_freeze_output = subprocess.check_output(['pip', 'freeze']).decode('utf-8')
+
+# # Write the output to a requirements.txt file
+# with open('requirements1.txt', 'w') as requirements_file:
+#     requirements_file.write(pip_freeze_output)
+
+# print("requirements1.txt file has been generated.")
