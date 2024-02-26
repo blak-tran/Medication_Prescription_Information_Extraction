c83f569ca67b16c290d1f120fa085a0c44394d34
blak-tran <ceilingdat@gmail.com>
2024-02-26 04:15:34 +0000

    fix create base model

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
-from app.core.base_model import UserDataBaseModel, MetaDataBaseModel
+from app.core.base_model import UserDataBaseModel, MetaDataBaseModel, MedicationBaseModel
 import os, cv2, io
 from PIL import Image
 from uuid import uuid4
@@ -16,6 +16,8 @@ def load_environments():
 load_environments()
 TEMP_ROOT_PATH = os.getenv("temp_path")
 temp_img_path = os.path.join(TEMP_ROOT_PATH,"temp_img.png")
+# Define the list of date formats you expect
+DATES_FORMATS = ["%d-%m-%Y", "%d%m/%Y"]
 
 def get_datetime_with_timezone():
     # Get the current time in UTC
@@ -26,9 +28,6 @@ def get_datetime_with_timezone():
 
     # Add the timezone offset to the current time
     datetime_with_timezone = current_time_utc.replace(tzinfo=timezone.utc) + timezone_offset
-
-    # Extract the date without the time information
-    date_only = datetime_with_timezone.date()
 
     return datetime_with_timezone
 
@@ -47,26 +46,45 @@ def parse_date_with_flexible_formats(date_str, formats):
             continue
     return None
 
-# Define the list of date formats you expect
-date_formats = ["%d-%m-%Y", "%d%m/%Y"]
+def calculate_drink_duration(data: list[MedicationBaseModel]):
+    calculation_base_date = get_datetime_with_timezone()
+
+    for record in data:
+        start_date = calculation_base_date
+
+        if record.dosage_per_day > 0:
+            num_days = record.total_quantity / record.dosage_per_day
+            end_date = start_date + timedelta(days=round(num_days))
+
+            # Assuming direct assignment is valid and expected to update the model
+            record.start_date = start_date.strftime("%d-%m-%Y")
+            record.end_date = end_date.strftime("%d-%m-%Y")
+        
+    return data
 
 # Function to create an instance of BaseModel from form_json
 def create_base_model(medication_records: list = None, 
                       meta_data: MetaDataBaseModel = None, 
                       prescription_Id: str = None):
-    for idx, _ in enumerate(medication_records.items):
-        medication_records.items[idx].record_id = str(uuid4())
-        if medication_records.items[idx].start_date is not None:
-            medication_records.items[idx].start_date = parse_date_with_flexible_formats(medication_records.items[idx].start_date, date_formats)
-        if medication_records.items[idx].end_date is not None:
-            medication_records.items[idx].end_date = parse_date_with_flexible_formats(medication_records.items[idx].end_date, date_formats)
     
-    if meta_data.created_at is not None:
-        meta_data.created_at = parse_date_with_flexible_formats(meta_data.created_at, date_formats)
-    if meta_data.modified_at is not None:
-        meta_data.modified_at = parse_date_with_flexible_formats(meta_data.modified_at, date_formats)
+    medication_records_full_info = calculate_drink_duration(medication_records.items)
     
-    return UserDataBaseModel(medication_records_id=prescription_Id, medication_records=medication_records.items, meta_data=meta_data)
+    for idx, _ in enumerate(medication_records_full_info):
+        medication_records_full_info[idx].record_id = str(uuid4())
+        medication_records_full_info[idx].start_date = parse_date_with_flexible_formats(str(medication_records_full_info[idx].start_date), DATES_FORMATS)
+        medication_records_full_info[idx].end_date = parse_date_with_flexible_formats(str(medication_records_full_info[idx].end_date), DATES_FORMATS)
+    
+    
+    if meta_data.created_at is None:
+        meta_data.created_at = get_datetime_with_timezone()
+    meta_data.created_at = parse_date_with_flexible_formats(meta_data.created_at, DATES_FORMATS)
+            
+    
+    if meta_data.modified_at is None:
+        meta_data.modified_at = get_datetime_with_timezone()
+    meta_data.modified_at = parse_date_with_flexible_formats(meta_data.modified_at, DATES_FORMATS)
+    
+    return UserDataBaseModel(medication_records_id=prescription_Id, medication_records=medication_records_full_info, meta_data=meta_data)
 
 def save_Image_from_bytes(encoded_image):
     # Decode the base64-encoded image
