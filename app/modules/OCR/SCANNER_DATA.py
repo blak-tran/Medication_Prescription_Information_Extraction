32e7053e40412ee2fd68ce855a4c01c66bc33936
blak-tran <ceilingdat@gmail.com>
2024-02-26 03:17:08 +0000

    update sorted data

--- app/modules/OCR/SCANNER_DATA.py
+++ app/modules/OCR/SCANNER_DATA.py
@@ -58,7 +58,7 @@ class APP_SCANNER:
         #     config_path=self.det_config,
         #     weight_path=self.det_weight)
         
-        self.det_model = MMOCRInferencer(det='DBNet', rec='SAR', kie='SDMGR', device='cuda:0')
+        self.det_model = MMOCRInferencer(det='DBNet', device='cuda:0')
         self.ocr_model = OCR(
             config_path=self.ocr_config,
             weight_path=self.ocr_weight)
@@ -89,63 +89,45 @@ class APP_SCANNER:
     def tracking_data(self, img):
         # Document extraction
         # img1 = self.preproc(img)
+        det_results = self.det_model(img, show=False)
+        det_polygons = det_results["predictions"][0]['det_polygons']
+        boxes = []
 
-        if self.debug:
-            det_results = self.det_model(img, show=False)
-            det_polygons = det_results["predictions"][0]['det_polygons']
-            boxes = []
-    
-            for polygon in det_polygons:
-                bbox = mmocr.utils.poly2bbox(polygon)
-                boxes.append(bbox)
+        for polygon in det_polygons:
+            bbox = mmocr.utils.poly2bbox(polygon)
+            boxes.append(bbox)
 
-            if self.cache_folder is None:
-                assert self.cache_folder, "Please specify output_path"
-            else:
-                output_path_crop = os.path.join(self.cache_folder, 'crops')
-                if os.path.exists(output_path_crop):
-                    shutil.rmtree(output_path_crop)
-                    os.mkdir(output_path_crop)
-            
-            def draw_bbox(img_path, result, color=(255, 0, 0), thickness=2):
-                if isinstance(img_path, str):
-                    img = cv2.imread(img_path)
-                    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                else:
-                    img = img_path.copy()
-                
-                for box in result:
-                    box = box.astype(int)
-                    # Define points as top-left, top-right, bottom-right, bottom-left
-                    tl, tr, br, bl = (box[0], box[1]), (box[2], box[1]), (box[2], box[3]), (box[0], box[3])
-                    
-                    # Draw lines between the points
-                    cv2.line(img, tl, tr, color, thickness)
-                    cv2.line(img, tr, br, color, thickness)
-                    cv2.line(img, br, bl, color, thickness)
-                    cv2.line(img, bl, tl, color, thickness)
-                    
-                return img
-            img1 = draw_bbox(img, boxes)
-            
-            self.crop_box(img, boxes, output_path_crop)
-
-            saved_img = cv2.cvtColor(img1, cv2.COLOR_RGB2BGR)
-            cv2.imwrite(self.preprocess_cache, saved_img)
-
-            # boxes, img2 = self.det_model(
-            #     img,
-            #     crop_region=True,
-            #     return_result=True,
-            #     output_path=self.cache_folder)
-            # saved_img = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
-            # cv2.imwrite(self.detection_cache, saved_img)
+        boxes = self.sort_boxes(boxes)
+        
+        if self.cache_folder is None:
+            assert self.cache_folder, "Please specify output_path"
         else:
-            boxes = self.det_model(
-                img,
-                crop_region=True,
-                return_result=False,
-                output_path=self.cache_folder)
+            output_path_crop = os.path.join(self.cache_folder, 'crops')
+            if os.path.exists(output_path_crop):
+                shutil.rmtree(output_path_crop)
+                os.mkdir(output_path_crop)
+        
+        
+        img1 = self.draw_bbox(img, boxes)
+        
+        self.crop_box(img, boxes, output_path_crop)
+
+        saved_img = cv2.cvtColor(img1, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(self.preprocess_cache, saved_img)
+
+        # boxes, img2 = self.det_model(
+        #     img,
+        #     crop_region=True,
+        #     return_result=True,
+        #     output_path=self.cache_folder)
+        # saved_img = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
+        # cv2.imwrite(self.detection_cache, saved_img)
+        # else:
+        #     boxes = self.det_model(
+        #         img,
+        #         crop_region=True,
+        #         return_result=False,
+        #         output_path=self.cache_folder)
 
         img_paths = os.listdir(self.crop_cache)
         img_paths.sort(key=natural_keys)
@@ -166,7 +148,52 @@ class APP_SCANNER:
             labels=preds, probs=probs,
             visualize_best=self.do_retrieve)
         
-        return self.final_output, texts
+        return self.final_output, " ".join(texts)
+    def draw_bbox(self, img_path, result, color=(255, 0, 0), thickness=2):
+        if isinstance(img_path, str):
+            img = cv2.imread(img_path)
+            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        else:
+            img = img_path.copy()
+        
+        for box in result:
+            box = box.astype(int)
+            # Define points as top-left, top-right, bottom-right, bottom-left
+            tl, tr, br, bl = (box[0], box[1]), (box[2], box[1]), (box[2], box[3]), (box[0], box[3])
+            
+            # Draw lines between the points
+            cv2.line(img, tl, tr, color, thickness)
+            cv2.line(img, tr, br, color, thickness)
+            cv2.line(img, br, bl, color, thickness)
+            cv2.line(img, bl, tl, color, thickness)
+            
+        return img
+    
+    def sort_boxes(self, boxes):
+        # First, sort by the top y-coordinate to ensure we process top-to-bottom
+        boxes_sorted_by_y = sorted(boxes, key=lambda box: box[1])
+        
+        # Now, sort each line by the x-coordinate
+        sorted_boxes = []
+        current_line = []
+        line_threshold = 10  # Threshold to consider boxes are in the same line
+        
+        for box in boxes_sorted_by_y:
+            if not current_line:
+                current_line.append(box)
+            else:
+                # If the vertical distance between boxes is small enough, consider them as the same line
+                if abs(box[1] - current_line[-1][1]) <= line_threshold:
+                    current_line.append(box)
+                else:
+                    # Sort the current line by x-coordinate and add to the sorted list, then start a new line
+                    sorted_boxes.extend(sorted(current_line, key=lambda box: box[0]))
+                    current_line = [box]
+        
+        # Don't forget to add the last line after sorting
+        sorted_boxes.extend(sorted(current_line, key=lambda box: box[0]))
+
+        return sorted_boxes
     
     def crop_box(self, img, boxes, out_folder, sort=True):
         h, w, c = img.shape
